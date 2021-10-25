deb582709f08164f6bf566c8057273a80d4310e6
raikeeen <sonnikcs@mail.ru>
2021-10-25 10:14:59 +0300

    fix api route

--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Models\Product;
 use App\Http\Controllers\ProductController;
+use App\Http\Controllers\API\ApiProductController;
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -18,8 +19,8 @@ Route::get('/products', function (){
     return Product::all();
 });
 //Route::get('/products/{id}', [ProductController::class,'show']);
-Route::resource('/products', App\Http\Controllers\Api\ApiProductController::class);
-Route::get('/products/search/{name}', [App\Http\Controllers\Api\ApiProductController::class, 'search']);
+Route::resource('/products', ApiProductController::class);
+Route::get('/products/search/{name}', [ApiProductController::class, 'search']);
 
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
