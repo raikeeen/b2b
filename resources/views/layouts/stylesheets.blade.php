c19a3c05122408511ebb1b8f60c289e63d4ece24
raikeeen <sonnikcs@mail.ru>
2021-11-10 17:09:06 +0200

    fix http fonts

--- resources/views/layouts/stylesheets.blade.php
+++ resources/views/layouts/stylesheets.blade.php
@@ -1,4 +1,4 @@
-<link href='http://fonts.googleapis.com/css?family=Roboto' rel='stylesheet' type='text/css'>
+<link href='https://fonts.googleapis.com/css?family=Roboto' rel='stylesheet' type='text/css'>
 {{--<link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">--}}
 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css">
 <link rel="stylesheet" href="{{asset('js/owl.carousel.css')}}">
