c8675afec77bc99f1bc9e08fd9be09fde0f9ee54
raikeeen <sonnikcs@mail.ru>
2022-01-14 09:57:55 +0200

    test

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Jobs\SendMail;
 use App\Models\Address;
 use App\Models\Brand;
 use App\Models\Category;
--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,10 @@
 <?php
 
+use App\Jobs\SendMail;
 use App\Models\B1Api;
 use App\Models\Product;
 use Illuminate\Support\Carbon;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\CartController;
@@ -17,6 +19,8 @@ use App\Http\Controllers\DocumentController;
 use App\Http\Controllers\Auth\UserController;
 use App\Http\Controllers\TecDocController;
 use App\Models\AjsApi;
+use Illuminate\Support\Facades\Storage;
+
 ini_set('max_execution_time', 300);
 /*
 |--------------------------------------------------------------------------
@@ -880,7 +884,37 @@ Route::group(['middleware' => 'auth'], function () {
         }
     });
     Route::get('/test1', function () {
-        //AjsApi::synchronizationStock();
+        $file = Storage::disk('ftp-maxgear')->get('STANY.csv');
+        Storage::disk('local')->put('/public/download/maxgear_stock.csv', $file);
+        $start = microtime(true);
+        $filename = public_path().'/storage/download/maxgear_stock.csv';
+        $file = fopen($filename, "r");
+        \DB::table('product')->where('supplier_id', 3)->update(array('stock_supplier' => 0));
+
+        if ($file) {
+            while (($line = fgets($file)) !== false) {
+                $line = explode(';', $line);
+
+                $product = DB::table('product')
+                    ->select(['supplier_reference','stock_supplier'])
+                    ->where('supplier_reference', $line[0])
+                    ->orWhere('reference', $line[0])
+                    ->first();
+                if(isset($product)) {
+                    DB::table('product')
+                        ->select(['supplier_reference', 'stock_supplier'])
+                        ->where('supplier_reference', $line[0])
+                        ->orWhere('reference', $line[0])
+                        ->update([
+                            'stock_supplier' => $product->stock_supplier + $line[1]
+                        ]);
+                }
+            }
+            fclose($file);
+            SendMail::dispatch(['name' => 'Maxgear stocks success', 'time' => 'Время выполнения скрипта: '.round(microtime(true) - $start, 4).' сек.'])->onQueue('mail');
+        } else {
+            // error opening the file.
+        }
     });
     Route::resource('/products',ProductController::class);
     Route::get('/new-products',[ProductController::class, 'newProduct'])->name('product.new');
